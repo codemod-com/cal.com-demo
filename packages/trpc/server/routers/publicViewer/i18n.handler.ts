2de07f8e5225b23d8ca35fb0b4638d122f19aaa0
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-09-21 13:12:00 +0200

    implementation of new locale calculated server-side

--- packages/trpc/server/routers/publicViewer/i18n.handler.ts
+++ packages/trpc/server/routers/publicViewer/i18n.handler.ts
@@ -11,7 +11,7 @@ type I18nOptions = {
   input: I18nInputSchema;
 };
 
-export const i18nHandler = async ({ input }: I18nOptions) => {
+export const i18nHandler = async ({ ctx, input }: I18nOptions) => {
   const { locale } = input;
   const { serverSideTranslations } = await import("next-i18next/serverSideTranslations");
   const i18n = await serverSideTranslations(locale, ["common", "vital"]);
