0a81a3e45764d9ce9a6a6cebb196ab266f6e5b61
dhryshyn <dev.dmytroh@gmail.com>
2023-09-28 14:21:36 +0300

    fix canonical

--- apps/web/app/event-types-1/page.tsx
+++ apps/web/app/event-types-1/page.tsx
@@ -1,21 +1,24 @@
 import EventTypesPage from "@pages/event-types";
-import type { ResolvingMetadata } from "next";
+import type { Metadata } from "next";
 
 import { getLayout } from "@calcom/features/MainLayout";
+import { IS_CALCOM, WEBAPP_URL } from "@calcom/lib/constants";
 
 import PageWrapper from "@components/PageWrapperAppDir";
-import { buildMetadata } from "@components/PageWrapperMetadata";
 
-export const generateMetadata = async (_: unknown, resolvingMetadata: ResolvingMetadata) => {
-  const resolvedMetadata = await resolvingMetadata;
-
-  return {
-    ...resolvedMetadata,
-    ...buildMetadata(""),
-  };
+export const metadata: Metadata = {
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
+  metadataBase: new URL(IS_CALCOM ? "https://cal.com" : WEBAPP_URL),
+  alternates: {
+    canonical: "/event-types",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "@calcom",
+  },
 };
 
-export default function EventTypesPageWrapped(props) {
+export default function EventTypesPageWrapped(props: Record<string, unknown>) {
   const wrapperProps = {
     Component: {
       getLayout,
