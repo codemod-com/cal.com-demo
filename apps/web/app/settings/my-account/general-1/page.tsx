38d1eeba4de392ce3f25d7f7b58b5c69c7b46621
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-10-02 15:41:40 +0200

    remove components.tsx

--- apps/web/app/settings/my-account/general-1/page.tsx
+++ apps/web/app/settings/my-account/general-1/page.tsx
@@ -1,11 +1,10 @@
 // This file has been sourced from: /intuita/cal.com-demo/apps/web/pages/settings/my-account/general.tsx
+import Components from "@pages/settings/my-account/general";
 import { headers } from "next/headers";
 
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
 
 import PageWrapper from "@components/PageWrapperAppDir";
-
-import Components from "./components";
 
 export default function General(props: Record<string, unknown>) {
   const h = headers();
