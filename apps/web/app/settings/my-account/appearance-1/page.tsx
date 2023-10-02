5722f0f60fb40ebc28f7d8e4a0f880d85ee7fe74
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-10-02 15:48:46 +0200

    remove components.tsx

--- apps/web/app/settings/my-account/appearance-1/page.tsx
+++ apps/web/app/settings/my-account/appearance-1/page.tsx
@@ -1,11 +1,9 @@
-// This file has been sourced from: /intuita/cal.com-demo/apps/web/pages/settings/my-account/appearance.tsx
+import Components from "@pages/settings/my-account/appearance";
 import { headers } from "next/headers";
 
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
 
 import PageWrapper from "@components/PageWrapperAppDir";
-
-import Components from "./components";
 
 export default async function Appearance(props: Record<string, unknown>) {
   const h = headers();
--- apps/web/app/settings/my-account/calendars-1/page.tsx
+++ apps/web/app/settings/my-account/calendars-1/page.tsx
@@ -1,11 +1,10 @@
 // This file has been sourced from: /intuita/cal.com-demo/apps/web/pages/settings/my-account/calendars.tsx
+import Components from "@pages/settings/my-account/calendars";
 import { headers } from "next/headers";
 
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
 
 import PageWrapper from "@components/PageWrapperAppDir";
-
-import Components from "./components";
 
 export default async function Page(props: Record<string, unknown>) {
   const h = headers();
