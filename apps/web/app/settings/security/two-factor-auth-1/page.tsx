0a5962606928e31c69cfaa3ebde595e4f93f1e67
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-10-02 15:28:28 +0200

    migrate /settings/security/two-factor-auth

---
+++ apps/web/app/settings/security/two-factor-auth-1/page.tsx
@@ -0,0 +1,24 @@
+import { headers } from "next/headers";
+
+import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
+
+import PageWrapper from "@components/PageWrapperAppDir";
+
+import Components from "./components";
+
+export default async function TwoFactorAuth(props: Record<string, unknown>) {
+  const h = headers();
+
+  const nonce = h.get("x-nonce") ?? undefined;
+
+  return (
+    <PageWrapper
+      getLayout={getLayout}
+      requiresLicense={false}
+      pageProps={props}
+      nonce={nonce}
+      themeBasis={null}>
+      <Components {...props} />
+    </PageWrapper>
+  );
+}
