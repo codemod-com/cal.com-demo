48efbdebb98b0423d3506835ff061f6e07d4fe20
Benny Joo <sldisek783@gmail.com>
2023-10-03 16:40:17 +0100

    logs

--- apps/web/app/error.tsx
+++ apps/web/app/error.tsx
@@ -9,11 +9,13 @@ import type { ErrorProps } from "next/error";
 import React from "react";
 
 import { HttpError } from "@calcom/lib/http-error";
+import logger from "@calcom/lib/logger";
 import { redactError } from "@calcom/lib/redactError";
 
 import { ErrorPage } from "@components/error/error-page";
 
 type NextError = Error & { digest?: string };
+
 // Ref: https://nextjs.org/docs/app/api-reference/file-conventions/error#props
 export type DefaultErrorProps = {
   error: NextError;
@@ -21,6 +23,8 @@ export type DefaultErrorProps = {
 };
 
 type AugmentedError = NextError | HttpError | null;
+
+const log = logger.getChildLogger({ prefix: ["[error]"] });
 
 type CustomErrorProps = {
   err?: AugmentedError;
@@ -47,6 +51,10 @@ const CustomError: NextPage<DefaultErrorProps> = (props) => {
       },
     };
   }
+
+  // `error.digest` property contains an automatically generated hash of the error that can be used to match the corresponding error in server-side logs
+  log.debug(`${error?.toString() ?? JSON.stringify(error)}`);
+  log.info("errorObject: ", errorObject);
 
   return (
     <ErrorPage statusCode={errorObject.statusCode} error={errorObject.err} message={errorObject.message} />
