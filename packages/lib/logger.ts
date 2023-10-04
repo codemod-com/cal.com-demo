a613c50f9c482dbe7183c721d8274696b040437a
Benny Joo <sldisek783@gmail.com>
2023-10-04 15:04:14 +0100

    fix

--- packages/lib/logger.ts
+++ packages/lib/logger.ts
@@ -1,21 +1,23 @@
 import { Logger } from "tslog";
 
-// import { IS_PRODUCTION } from "./constants";
+import { IS_PRODUCTION } from "./constants";
 
 const logger = new Logger({
   minLevel: !!process.env.NEXT_PUBLIC_DEBUG ? 2 : 4,
   maskValuesOfKeys: ["password", "passwordConfirmation", "credentials", "credential"],
+  prettyLogTimeZone: IS_PRODUCTION ? "UTC" : "local",
   // dateTimePattern: "hour:minute:second.millisecond",
   // displayFunctionName: false,
   // displayFilePath: "hidden",
-  // dateTimeTimezone: IS_PRODUCTION ? "utc" : Intl.DateTimeFormat().resolvedOptions().timeZone,
-  // prettyInspectHighlightStyles: {
-  //   name: "yellow",
-  //   number: "blue",
-  //   bigint: "blue",
-  //   boolean: "blue",
-  // },
   // exposeErrorCodeFrame: !IS_PRODUCTION,
+  stylePrettyLogs: true,
+  prettyLogStyles: {
+    name: "yellow",
+    dateIsoStr: "blue",
+    // number: "blue",
+    // bigint: "blue",
+    // boolean: "blue",
+  },
 });
 
 export default logger;
