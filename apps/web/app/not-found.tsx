c4ab0ba47b1934fa67888efbb0379830ed18cd03
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-10-11 13:44:05 +0300

    add i18n patch

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,19 +1,15 @@
 import NotFoundPage from "@pages/legacy_404";
+import config from "next-i18next.config";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { headers } from "next/headers";
-
-import { CALCOM_VERSION } from "@calcom/lib/constants";
 
 import PageWrapper from "@components/PageWrapperAppDir";
 
 const getProps = async () => {
-  // until ssgInit is migrated
-  const _i18n = await fetch(
-    `https://app.cal.com/api/trpc/public/i18n?input={"json":{"locale":"en","CalComVersion":"${CALCOM_VERSION}"}}`,
-    { cache: "no-cache" }
-  );
+  const i18n = await serverSideTranslations("en", undefined, config);
 
   return {
-    i18n: (await _i18n.json()).result.data.json.i18n,
+    i18n,
   };
 };
 
@@ -23,7 +19,6 @@ const NotFound = async () => {
   const nonce = h.get("x-nonce") ?? undefined;
 
   const { i18n } = await getProps();
-  console.log(i18n, "?");
   return (
     // @ts-expect-error withTrpc expects AppProps
     <PageWrapper requiresLicense={false} pageProps={{ i18n }} nonce={nonce} themeBasis={null} i18n={i18n}>
