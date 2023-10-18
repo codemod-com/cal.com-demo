6665b7990cac74da20a47b5e50d99280a66a9d5c
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-10-18 12:52:53 +0300

    chore: bootstrapping phase

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -58,7 +58,7 @@ export default function Custom404() {
     });
 
     const [routerUsername] = pathname?.replace("%20", "-").split(/[?#]/) ?? [];
-    if (routerUsername && (!isValidOrgDomain || !currentOrgDomain)) {
+    if (!isValidOrgDomain || !currentOrgDomain) {
       const splitPath = routerUsername.split("/");
       if (splitPath[1] === "team" && splitPath.length === 3) {
         // Accessing a non-existent team
