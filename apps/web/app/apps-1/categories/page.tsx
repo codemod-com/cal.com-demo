bf8eeed1594bab5929293b57a5ec0964a18f3d57
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-10-26 13:00:48 +0300

    wip

--- apps/web/app/apps-1/categories/page.tsx
+++ apps/web/app/apps-1/categories/page.tsx
@@ -1,3 +1,18 @@
-export default async function Page() {
-  return <>Page</>;
+import LegacyPage, { getServerSideProps } from "@pages/apps/categories/index";
+import { cookies, headers } from "next/headers";
+
+type Params = {
+  [key: string]: string | string[] | undefined;
+};
+
+async function getPageProps({ params }: { params: Params }) {
+  // @ts-expect-error context
+  const result = await getServerSideProps({ params, req: { headers: headers(), cookies: cookies() } });
+  return result.props;
+}
+
+export default async function Page({ params }: { params: Params }) {
+  const props = await getPageProps({ params });
+
+  return <LegacyPage {...props} />;
 }
