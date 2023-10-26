2b8bf2319af1df81e405a1c819b5b0edd868077a
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-10-26 12:27:28 +0300

    wip

--- apps/web/app/apps-1/[slug]/setup/page.tsx
+++ apps/web/app/apps-1/[slug]/setup/page.tsx
@@ -1,3 +1,25 @@
-export default async function Page() {
-  return null;
+import SetupPage from "@pages/apps/[slug]/setup";
+import { cookies, headers } from "next/headers";
+import { notFound, redirect } from "next/navigation";
+
+import { getServerSideProps } from "@calcom/app-store/_pages/setup/_getServerSideProps";
+
+const getPageProps = async ({ params }: { params: Record<string, string | string[]> }) => {
+  // @ts-expect-error @TODO check
+  const result = await getServerSideProps({ params, req: { headers: headers(), cookies: cookies() } });
+
+  if (!result || "notFound" in result) {
+    notFound();
+  }
+
+  if ("redirect" in result) {
+    redirect(result.redirect.destination);
+  }
+
+  return result.props;
+};
+
+export default async function Page({ params }: { params: Record<string, string | string[]> }) {
+  const pageProps = await getPageProps({ params });
+  return <SetupPage {...pageProps} />;
 }
