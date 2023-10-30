2257aef376f4bdcd1dbb84a18cb2b4690a053bd9
Benny Joo <sldisek783@gmail.com>
2023-10-30 17:50:24 +0000

    Move /impersonation dir to correct dir and add metadata

---
+++ apps/web/app/settings/security/impersonation/page.tsx
@@ -0,0 +1,39 @@
+import ImpersonationPage from "@pages/settings/security/impersonation";
+import { headers } from "next/headers";
+
+import { constructGenericImage } from "@calcom/lib/OgImages";
+import { IS_CALCOM, WEBAPP_URL, APP_NAME, SEO_IMG_OGIMG } from "@calcom/lib/constants";
+import { getFixedT } from "@calcom/lib/server/getFixedT";
+
+import { preparePageMetadata } from "@lib/metadata";
+
+export const generateMetadata = async () => {
+  const h = headers();
+  const canonical = h.get("x-pathname") ?? "";
+  const locale = h.get("x-locale") ?? "en";
+
+  const t = await getFixedT(locale, "common");
+
+  const title = t("impersonation");
+  const description = t("impersonation_description", { appName: APP_NAME });
+
+  const metadataBase = new URL(IS_CALCOM ? "https://cal.com" : WEBAPP_URL);
+
+  const image =
+    SEO_IMG_OGIMG +
+    constructGenericImage({
+      title,
+      description,
+    });
+
+  return preparePageMetadata({
+    title,
+    canonical,
+    image,
+    description,
+    siteName: APP_NAME,
+    metadataBase,
+  });
+};
+
+export default ImpersonationPage;
