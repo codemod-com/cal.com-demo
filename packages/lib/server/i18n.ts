c4d117b02cf5de788f0e19e3f8079d4fced15aed
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-10-30 14:29:50 +0200

    fix trpc-router-import-part-1

--- packages/lib/server/i18n.ts
+++ packages/lib/server/i18n.ts
@@ -1,8 +1,6 @@
-import { i18n as nexti18next } from "next-i18next";
-
 import { create } from "./getFixedT";
 
 export const getTranslation = async (locale: string, ns: string) => {
-  const _i18n = nexti18next != null ? nexti18next : await create(locale, ns);
+  const _i18n = await create(locale, ns);
   return _i18n.getFixedT(locale, ns);
 };
