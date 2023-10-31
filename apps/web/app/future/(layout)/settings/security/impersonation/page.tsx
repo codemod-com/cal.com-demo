bfe31e51f059b42fa5556a089f2668a2c80b184f
Benny Joo <sldisek783@gmail.com>
2023-10-31 20:05:43 +0000

    remove duplicates

--- apps/web/app/future/(layout)/settings/security/impersonation/page.tsx
+++ apps/web/app/future/(layout)/settings/security/impersonation/page.tsx
@@ -1,39 +1,7 @@
 import ImpersonationPage from "@pages/settings/security/impersonation";
-import { headers } from "next/headers";
+import { _generateMetadata } from "app/_utils";
 
-import { constructGenericImage } from "@calcom/lib/OgImages";
-import { IS_CALCOM, WEBAPP_URL, APP_NAME, SEO_IMG_OGIMG } from "@calcom/lib/constants";
-import { getFixedT } from "@calcom/lib/server/getFixedT";
-
-import { preparePageMetadata } from "@lib/metadata";
-
-export const generateMetadata = async () => {
-  const h = headers();
-  const canonical = h.get("x-pathname") ?? "";
-  const locale = h.get("x-locale") ?? "en";
-
-  const t = await getFixedT(locale, "common");
-
-  const title = t("impersonation");
-  const description = t("impersonation_description");
-
-  const metadataBase = new URL(IS_CALCOM ? "https://cal.com" : WEBAPP_URL);
-
-  const image =
-    SEO_IMG_OGIMG +
-    constructGenericImage({
-      title,
-      description,
-    });
-
-  return preparePageMetadata({
-    title,
-    canonical,
-    image,
-    description,
-    siteName: APP_NAME,
-    metadataBase,
-  });
-};
+export const generateMetadata = async () =>
+  await _generateMetadata("impersonation", "impersonation_description");
 
 export default ImpersonationPage;
