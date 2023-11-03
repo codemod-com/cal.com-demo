8415cea401428c811643c6b7bac64533cf10743e
Benny Joo <sldisek783@gmail.com>
2023-11-03 16:36:02 +0000

    fix

--- apps/web/app/_trpc/trpc-provider.tsx
+++ apps/web/app/_trpc/trpc-provider.tsx
@@ -1,18 +1,13 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type DehydratedState, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HydrateClient } from "app/_trpc/HydrateClient";
 import { trpc } from "app/_trpc/client";
-import { createTRPCNextLayout } from "app/_trpc/createTRPCNextLayout";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { headers } from "next/headers";
-import { use, useState } from "react";
+import { useState } from "react";
 import superjson from "superjson";
 
-import prisma from "@calcom/prisma";
 import { httpBatchLink } from "@calcom/trpc/client/links/httpBatchLink";
 import { httpLink } from "@calcom/trpc/client/links/httpLink";
 import { loggerLink } from "@calcom/trpc/client/links/loggerLink";
 import { splitLink } from "@calcom/trpc/client/links/splitLink";
-import { appRouter } from "@calcom/trpc/server/routers/_app";
 
 const ENDPOINTS = [
   "admin",
@@ -65,7 +60,10 @@ const resolveEndpoint = (links: any) => {
   };
 };
 
-export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const TrpcProvider: React.FC<{ children: React.ReactNode; dehydratedState?: DehydratedState }> = ({
+  children,
+  dehydratedState,
+}) => {
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -120,24 +118,10 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({ children
     })
   );
 
-  const locale = headers().get("x-locale") ?? "en";
-
-  const i18n = use(serverSideTranslations(locale, ["common"])) ?? "en";
-
-  const helper = createTRPCNextLayout({
-    router: appRouter,
-    transformer: superjson,
-    createContext() {
-      return { prisma, session: null, locale, i18n };
-    },
-  });
-
-  const hydratedState = trpc.useDehydratedState(trpcClient, use(helper.dehydrate()));
-
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        {hydratedState ? <HydrateClient state={hydratedState}>{children}</HydrateClient> : children}
+        {dehydratedState ? <HydrateClient state={dehydratedState}>{children}</HydrateClient> : children}
       </QueryClientProvider>
     </trpc.Provider>
   );
