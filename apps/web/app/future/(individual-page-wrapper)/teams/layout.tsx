b0cd670a8261b92444b4cb6e07956c45153a8ebc
Benny Joo <sldisek783@gmail.com>
2023-11-06 16:21:13 +0000

    wip

--- apps/web/app/future/(individual-page-wrapper)/teams/layout.tsx
+++ apps/web/app/future/(individual-page-wrapper)/teams/layout.tsx
@@ -1,8 +1,10 @@
 import { ssrInit } from "app/_trpc/ssrInit";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
+import { type NextRequest } from "next/server";
 import { type ReactElement } from "react";
 
 import { getLayout } from "@calcom/features/MainLayoutAppDir";
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
 
 import PageWrapper from "@components/PageWrapperAppDir";
 
@@ -10,25 +12,37 @@ type LayoutProps = {
   children: ReactElement;
 };
 
-const getState = async () => {
+const getProps = async () => {
   const ssr = await ssrInit();
   await ssr.viewer.me.prefetch();
+  const req: NextRequest = { cookies: cookies(), headers: headers() };
+  const session = await getServerSession({
+    req,
+  });
 
-  return ssr.dehydrate();
+  // const token = Array.isArray(context.query?.token) ? context.query.token[0] : context.query?.token;
+  const token = null;
+  const callbackUrl = token ? `/teams?token=${encodeURIComponent(token)}` : null;
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: callbackUrl ? `/auth/login?callbackUrl=${callbackUrl}` : "/auth/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return { dehydratedState: await ssr.dehydrate() };
 };
 
 export default async function Layout({ children }: LayoutProps) {
   const h = headers();
   const nonce = h.get("x-nonce") ?? undefined;
-  const state = await getState();
+  const props = await getProps();
 
   return (
-    <PageWrapper
-      getLayout={getLayout}
-      requiresLicense={false}
-      nonce={nonce}
-      themeBasis={null}
-      dehydratedState={state}>
+    <PageWrapper getLayout={getLayout} requiresLicense={false} nonce={nonce} themeBasis={null} {...props}>
       {children}
     </PageWrapper>
   );
