acde0d0d53c8449fa03408ed6b6bbe88cf558332
Benny Joo <sldisek783@gmail.com>
2023-11-15 01:43:11 +0900

    Migrate reschedule/[uid]

--- apps/web/app/future/(shared-page-wrapper)/(no-layout)/reschedule/[uid]/page.tsx
+++ apps/web/app/future/(shared-page-wrapper)/(no-layout)/reschedule/[uid]/page.tsx
@@ -1,7 +1,8 @@
-import type { GetServerSidePropsContext } from "next";
+import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { cookies, headers } from "next/headers";
+import { redirect, notFound } from "next/navigation";
 import { URLSearchParams } from "url";
 import { z } from "zod";
-import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
 import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
 import { getDefaultEvent } from "@calcom/lib/defaultEvents";
@@ -12,17 +13,19 @@ type PageProps = Readonly<{
   params: Params;
 }>;
 
-export default function Type() {
-  // Just redirect to the schedule page to reschedule it.
+export default async function Page({ params }: PageProps) {
+  await getProps(params);
+
   return null;
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context);
+async function getProps(params: Params) {
+  const req = { headers: headers(), cookies: cookies() };
+  const session = await getServerSession({ req });
 
   const { uid: bookingUid, seatReferenceUid } = z
     .object({ uid: z.string(), seatReferenceUid: z.string().optional() })
-    .parse(context.query);
+    .parse(params);
 
   const { uid, seatReferenceUid: maybeSeatReferenceUid } = await maybeGetBookingUidFromSeat(
     prisma,
@@ -73,18 +76,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const dynamicEventSlugRef = booking?.dynamicEventSlugRef || "";
 
   if (!booking) {
-    return {
-      notFound: true,
-    } as const;
+    return notFound();
   }
 
   if (!booking?.eventType && !booking?.dynamicEventSlugRef) {
     // TODO: Show something in UI to let user know that this booking is not rescheduleable
-    return {
-      notFound: true,
-    } as {
-      notFound: true;
-    };
+    return notFound();
   }
 
   // if booking event type is for a seated event and no seat reference uid is provided, throw not found
@@ -92,12 +89,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     const userId = session?.user?.id;
 
     if (!userId && !seatReferenceUid) {
-      return {
-        redirect: {
-          destination: `/auth/login?callbackUrl=/reschedule/${bookingUid}`,
-          permanent: false,
-        },
-      };
+      return redirect(`/auth/login?callbackUrl=/reschedule/${bookingUid}`);
     }
     const userIsHost = booking?.eventType.hosts.find((host) => {
       if (host.user.id === userId) return true;
@@ -106,11 +98,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     const userIsOwnerOfEventType = booking?.eventType.owner?.id === userId;
 
     if (!userIsHost && !userIsOwnerOfEventType) {
-      return {
-        notFound: true,
-      } as {
-        notFound: true;
-      };
+      return notFound();
     }
   }
 
@@ -127,12 +115,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   destinationUrl.set("rescheduleUid", seatReferenceUid || bookingUid);
 
-  return {
-    redirect: {
-      destination: `/${eventPage}?${destinationUrl.toString()}${
-        eventType.seatsPerTimeSlot ? "&bookingUid=null" : ""
-      }`,
-      permanent: false,
-    },
-  };
+  return redirect(
+    `/${eventPage}?${destinationUrl.toString()}${eventType.seatsPerTimeSlot ? "&bookingUid=null" : ""}`
+  );
 }
