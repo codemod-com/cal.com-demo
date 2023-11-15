3ac4d7bce9b8284b3b3310e26ed21c70b4cd3e88
DmytroHryshyn <125881252+DmytroHryshyn@users.noreply.github.com>
2023-11-15 11:01:53 +0200

    fix-apps-slug-pages (#104)
    * fix-apps-slug-pages
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- apps/web/pages/apps/[slug]/[...pages].tsx
+++ apps/web/pages/apps/[slug]/[...pages].tsx
@@ -50,7 +50,6 @@ function getRoute(appName: string, pages: string[]) {
   }
   const mainPage = pages[0];
   const appPage = routingConfig.layoutHandler || (routingConfig[mainPage] as AppPageType);
-
   if (!appPage) {
     return {
       notFound: true,
@@ -62,7 +61,7 @@ function getRoute(appName: string, pages: string[]) {
 const AppPage: AppPageType["default"] = function AppPage(props) {
   const appName = props.appName;
   const params = useParamsWithFallback();
-  const pages = (params.pages || []) as string[];
+  const pages = Array.isArray(params.pages) ? params.pages : params.pages?.split("/") ?? [];
   const route = getRoute(appName, pages);
 
   const componentProps = {
