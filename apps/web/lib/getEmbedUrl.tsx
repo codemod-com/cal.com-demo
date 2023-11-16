ff90867af1e9020386b958eed560b663f5d10e7a
Benny Joo <sldisek783@gmail.com>
2023-11-16 11:34:01 +0000

    Migrate /reschedule/[uid]/embed.tsx

---
+++ apps/web/lib/getEmbedUrl.tsx
@@ -0,0 +1,32 @@
+import { type Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { redirect } from "next/navigation";
+
+import { WEBAPP_URL } from "@calcom/lib/constants";
+
+export default function getEmbedUrl(params: Params, destinationUrl: string) {
+  const embed = params.embed;
+  const layout = params.layout;
+  if (!embed || !layout) {
+    return destinationUrl;
+  }
+
+  let urlPrefix = "";
+
+  // Get the URL parsed from URL so that we can reliably read pathname and searchParams from it.
+  const destinationUrlObj = new URL(destinationUrl, WEBAPP_URL);
+
+  // If it's a complete URL, use the origin as the prefix to ensure we redirect to the same domain.
+  if (destinationUrl.search(/^(http:|https:).*/) !== -1) {
+    urlPrefix = destinationUrlObj.origin;
+  } else {
+    // Don't use any prefix for relative URLs to ensure we stay on the same domain
+    urlPrefix = "";
+  }
+
+  const destinationQueryStr = destinationUrlObj.searchParams.toString();
+  // Make sure that redirect happens to /embed page and pass on embed query param as is for preserving Cal JS API namespace
+  const newDestinationUrl = `${urlPrefix}${destinationUrlObj.pathname}/embed?${
+    destinationQueryStr ? `${destinationQueryStr}&` : ""
+  }layout=${layout}&embed=${embed}`;
+  return redirect(newDestinationUrl);
+}
