f751225c92493d16a53c14f6bd98b921cb071c3f
Benny Joo <sldisek783@gmail.com>
2023-11-29 17:55:32 +0000

    intuita codemod: app-directory-boilerplate-calcom

---
+++ apps/web/app/future/(shared-page-wrapper)/(layout)/availability/troubleshoot/page.tsx
@@ -0,0 +1,10 @@
+import Page from "@pages/availability/troubleshoot";
+import { _generateMetadata } from "app/_utils";
+
+export const generateMetadata = async () =>
+  await _generateMetadata(
+    () => "",
+    () => ""
+  );
+
+export default Page;
---
+++ apps/web/app/future/(shared-page-wrapper)/(no-layout)/availability/[schedule]/page.tsx
@@ -0,0 +1,10 @@
+import Page from "@pages/availability/[schedule]";
+import { _generateMetadata } from "app/_utils";
+
+export const generateMetadata = async () =>
+  await _generateMetadata(
+    () => "",
+    () => ""
+  );
+
+export default Page;
