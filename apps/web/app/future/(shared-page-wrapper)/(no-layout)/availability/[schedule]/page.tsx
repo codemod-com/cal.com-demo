49436fa751688b245ba2efdfb6658726b7ea081b
Benny Joo <sldisek783@gmail.com>
2023-11-29 18:14:34 +0000

    manual: add title/description metadata

--- apps/web/app/future/(shared-page-wrapper)/(no-layout)/availability/[schedule]/page.tsx
+++ apps/web/app/future/(shared-page-wrapper)/(no-layout)/availability/[schedule]/page.tsx
@@ -1,10 +1,23 @@
 import Page from "@pages/availability/[schedule]";
+import { type Params } from "app/_types";
 import { _generateMetadata } from "app/_utils";
+import { cookies, headers } from "next/headers";
 
-export const generateMetadata = async () =>
+export const generateMetadata = async ({ params }: { params: Params }) => {
+  const scheduleId = params.schedule ? Number(params.schedule) : -1;
+
+  // @ts-expect-error Type '{ headers: ReadonlyHeaders; cookies: ReadonlyRequestCookies; }' is not assignable to type 'NextApiRequest'
+  const data = await getServerCaller({ req, prisma }).viewer.availability.schedule.get({ scheduleId });
+  const { schedule } = data;
+  const req = {
+    headers: headers(),
+    cookies: cookies(),
+  };
+
   await _generateMetadata(
-    () => "",
-    () => ""
+    (t) => (schedule?.name ? `${schedule.name} | ${t("availability")}` : t("availability")),
+    (t) => (schedule?.name ? `${schedule.name} | ${t("availability")}` : t("availability"))
   );
+};
 
 export default Page;
--- apps/web/app/future/(shared-page-wrapper)/(troubleshoot-layout)/availability/troubleshoot/page.tsx
+++ apps/web/app/future/(shared-page-wrapper)/(troubleshoot-layout)/availability/troubleshoot/page.tsx
@@ -3,8 +3,8 @@ import { _generateMetadata } from "app/_utils";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
-    () => "",
-    () => ""
+    (t) => t("troubleshoot"),
+    (t) => t("troubleshoot_availability")
   );
 
 export default Page;
