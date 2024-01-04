cec96c15df6568fb26bfb0c583a4f657bdcd623f
DmytroHryshyn <125881252+DmytroHryshyn@users.noreply.github.com>
2024-01-04 12:37:44 +0200

    chore: remove router groups, add LayoutHOC (#156)
    * make no-meeting-found page use ssr
    
    * remove-route-groups
    
    * add LayoutHOC
    
    * fix
    
    * fix

---
+++ apps/web/app/layoutHOC.tsx
@@ -0,0 +1,26 @@
+import type { LayoutProps, PageProps } from "app/_types";
+import { cookies, headers } from "next/headers";
+
+import { buildLegacyCtx } from "@lib/buildLegacyCtx";
+
+import PageWrapper from "@components/PageWrapperAppDir";
+
+type WithLayoutParams<T extends Record<string, any>> = {
+  getLayout: ((page: React.ReactElement) => React.ReactNode) | null;
+  Page?: (props: T) => React.ReactElement;
+  getData?: (arg: ReturnType<typeof buildLegacyCtx>) => Promise<T>;
+};
+
+export function WithLayout<T extends Record<string, any>>({ getLayout, getData, Page }: WithLayoutParams<T>) {
+  return async ({ params, ...restProps }: PageProps | LayoutProps) => {
+    const h = headers();
+    const nonce = h.get("x-nonce") ?? undefined;
+    const props = getData ? await getData(buildLegacyCtx(h, cookies(), params)) : ({} as T);
+
+    return (
+      <PageWrapper getLayout={getLayout} requiresLicense={false} nonce={nonce} themeBasis={null} {...props}>
+        {Page ? <Page {...props} /> : "children" in restProps ? restProps.children : null}
+      </PageWrapper>
+    );
+  };
+}
