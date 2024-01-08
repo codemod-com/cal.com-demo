cde31800966848fcc10b509f0ba09539f351527d
Benny Joo <sldisek783@gmail.com>
2024-01-08 16:16:19 +0000

    import type

--- apps/web/app/future/team/[slug]/embed/page.tsx
+++ apps/web/app/future/team/[slug]/embed/page.tsx
@@ -1,15 +1,11 @@
 import { getServerSideProps } from "@pages/team/[slug]";
 import { withAppDir } from "app/AppDirSSRHOC";
+import type { PageProps } from "app/_types";
 import type { GetServerSidePropsContext } from "next";
-import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { cookies, headers } from "next/headers";
 
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 import withEmbedSsr from "@lib/withEmbedSsr";
-
-type PageProps = Readonly<{
-  params: Params;
-}>;
 
 const Page = async ({ params }: PageProps) => {
   const legacyCtx = buildLegacyCtx(headers(), cookies(), params);
