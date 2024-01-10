85ccb5ba1dac3b95c5a6ef36404f4eae9e5e3a3a
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2024-01-10 17:54:02 +0100

    search params fixes

--- apps/web/app/layoutHOC.tsx
+++ apps/web/app/layoutHOC.tsx
@@ -22,8 +22,13 @@ export function WithLayout<T extends Record<string, any>>({
   return async <P extends "P" | "L">(p: P extends "P" ? PageProps : LayoutProps) => {
     const h = headers();
     const nonce = h.get("x-nonce") ?? undefined;
+
+    const searchParams = "searchParams" in p ? p.searchParams : {};
+
     const props = getData
-      ? await getData(buildLegacyCtx(h, cookies(), p.params) as unknown as GetServerSidePropsContext)
+      ? await getData(
+          buildLegacyCtx(h, cookies(), p.params, searchParams) as unknown as GetServerSidePropsContext
+        )
       : ({} as T);
 
     const children = "children" in p ? p.children : null;
